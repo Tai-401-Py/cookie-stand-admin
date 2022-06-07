b07755c6c7c30d2eae551022ceb7c6ab93d18e0a
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-07 15:15:52 -0700

    added header:

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <>
+        <header className='bg-green-400 py-6 pl-6 text-2xl'>
+          <h1 className=''>Cookie Stand Admin</h1>
+        </header>
+      </>
+    )
+  }
+
+export default Header
