2973505c1771d8578e1d8a4c3917931545604420
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-08 14:15:53 -0700

    added overview button

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,12 @@
 const Header = () => {
-    return (
-      <>
-        <header className='bg-green-400 py-6 pl-6 text-2xl'>
-          <h1 className=''>Cookie Stand Admin</h1>
-        </header>
-      </>
-    )
-  }
+  return (
+    <>
+      <header className='bg-emerald-500 py-6 pl-6 flex'>
+        <h1 className='text-2xl'>Cookie Stand Admin</h1>
+        <button className='bg-gray-200 hover:bg-gray-500 text-sm border rounded ml-auto mr-6'>Overview</button>
+      </header>
+    </>
+  )
+}
 
 export default Header
