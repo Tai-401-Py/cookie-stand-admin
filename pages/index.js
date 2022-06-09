f5d5dd8bcec8ecbe0ea4d67f186a7e47bc4a586c
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-09 14:06:20 -0700

    push to pull

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useAuth } from '../contexts/auth';
+import useResource from '../hooks/useResource';
 import CookieStandAdmin from '../components/CookieStandAdmin'
 
 export default function Home() {
+
+  const { user, login } = useAuth();
   return (
     <>
+    <Head>
+      <title>Cookie Stand Admin</title>
+    </Head>
+    {user? 
       <CookieStandAdmin/>
+      :
+      <LoginForm onLogin={login}/>
+    }
     </>
   )
 }
