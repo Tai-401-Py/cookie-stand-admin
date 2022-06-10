6bff11616998bb8c029f6973a842306f7b7874de
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-09 18:55:51 -0700

    fixed passing of props

--- components/Main.js
+++ components/Main.js
@@ -1,66 +1,26 @@
 import SubmissionForm from '../components/SubmissionForm'
 import SalesTable from '../components/SalesTable';
-import { useState } from 'react'
+
+import useResource from '../hooks/useResource'
 
 const Main = (props) => {
+  
+  const { resources, deleteResource } = useResource();
 
-    const [report, setReport] = useState([])
-    const [hourlyTotal, setHourlyTotal] = useState([])
 
-    const calHourlyTotal = (report) => {
-      let total = 0
-      let result = []
-      for (let i = 0; i < 14; i++){
-          for(let j = 0; j < report.length; j++){
-              total += report[j].hourlySales[i];
-          }
-          result.push(total)
-          total = 0
-        }
-      setHourlyTotal(result)
-    }
-    
-    function calSalesTotal(store){
-      
-      let sales = []
-      for (let i = 0; i < 14; i++){
-        let rand  = Math.random()
-        let sum = rand*(parseInt(store.maxCustomers)-parseInt(store.minCustomers)+1) 
-        sum += parseInt(store.minCustomers)
-        let custmer = Math.floor(parseInt(sum))
-        let cookie = custmer*parseFloat(store.avgCookies)
-        sales.push(Math.floor(cookie))
-      }
-      return sales
-    }
-    
-    
-    function createData(values){
-      console.log(values)
-      console.log(report)
-      values.hourlySales =  calSalesTotal(values)
-      let newarr = report
-      newarr.push(values)
-      setReport(newarr)
-      calHourlyTotal(newarr)
-      console.log(hourlyTotal)
-        }
-      
-
-    return (
-      <>
-        <main className='bg-emerald-300 w-1/2 flex flex-col items-center mx-auto rounded my-6'>
-          <SubmissionForm
-          createData = {createData}
-          setNumLoc = {props.setNumLoc}
-          />
-        </main>
-        <SalesTable
-        report = {report}
-        hourlyTotal = {hourlyTotal}
+  return (
+    <>
+      <main className='bg-emerald-300 w-1/2 flex flex-col items-center mx-auto rounded my-6'>
+        <SubmissionForm
+          setNumLoc={props.setNumLoc}
         />
-      </>
-    )
-  }
+      </main>
+      <SalesTable
+        stands={resources || []} 
+        deleteStand={deleteResource}
+      />
+    </>
+  )
+}
 
 export default Main
