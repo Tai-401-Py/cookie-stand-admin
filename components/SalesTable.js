6581433bdd231ae0d954c61f6ed529c925696b94
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-09 18:54:36 -0700

    Added useeffect to Table footer to render state chenge on addition or removal of data

--- components/SalesTable.js
+++ components/SalesTable.js
@@ -1,25 +1,31 @@
 import { hours } from '../data/Hours'
-
+import { useState, useEffect } from 'react'
 
 const SalesTable = (props) => {
 
-    return(
+    return (
 
         <div className="flex flex-col ... text-center ... mb-8 ... container mx-auto w-4/5">
-        {(props.report.length == 0) ?
-            <h2>No Cookie Stands Available</h2> :
-            <table className="border-collapse rounded w-3/4 mx-auto">
-                <TableHeader/>
-                <TableBody
-                report = {props.report}
-                />
-                <TableFooter
-                report = {props.report}
-                hourlyTotal = {props.hourlyTotal}
-                />
-            </table>
-        }
-    </div>
+            {(props.stands.length == 0) ?
+                <h2>No Cookie Stands Available</h2> :
+                <table className="border-collapse rounded w-3/4 mx-auto">
+
+                    <TableHeader />
+
+                    <tbody>
+                        {props.stands.map(stand => (
+                            <TableBody
+                                key={stand.id}
+                                stand={stand}
+                                deleteStand={props.deleteStand}
+                            />))}
+                    </tbody>
+                        <TableFooter
+                        resources = {props.stands}
+                        />
+                </table>
+            }
+        </div>
     )
 }
 
@@ -44,34 +50,57 @@ const TableHeader = () => {
 
 const TableBody = (props) => {
 
+    function clickHandler() {
+        props.deleteStand(props.stand.id);
+    }
+
+    if (props.stand.hourly_sales.length == 0) {
+        // bunk data
+        props.stand.hourly_sales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+
     return (
-        <tbody className="border-collapse border border-gray-900">
-            {props.report.map(data => (
-                <tr className="border-collapse border border-gray-900" key={data.id}>
-                    <td className="border-collapse border border-gray-900">
-                        <div className="flex justify-between">
-                            <div className="pl-2">
-                                {data.location}
-                            </div>
-                        </div>
-                    </td>
-                    {data.hourlySales.map(cookie => (<td className="border-collapse border border-gray-900">{cookie}</td>))}
-                    <td className="border-collapse border border-gray-900">{data.hourlySales.reduce((acc, curr) => { acc = acc + curr; return acc }, 0)}</td>
-                </tr>
-            ))}
-        </tbody>
-    )
+        <tr>
+            <td>
+                {props.stand.location}
+                <button onClick={clickHandler}>[x]</button>
+            </td>
+            {props.stand.hourly_sales.map((slot, index) =>
+                <td key={index}>{slot}</td>
+            )}
+            <td>
+                {props.stand.hourly_sales.reduce((num, sum) => num + sum, 0)}
+            </td>
+        </tr>
+    );
 }
 
 const TableFooter = (props) => {
 
+    const [totalSumArray, setTotalSum] = useState([])
+    useEffect(() => {
+        let total = 0
+        const result = []
+        for (let i = 0; i < 14; i++) {
+            for (let j = 0; j < props.resources.length; j++) {
+                total += props.resources[j].hourly_sales[i];
+            }
+            result.push(total)
+            total = 0
+        }
+        setTotalSum(
+            result
+        )
+    }, [props.resources.length]);
+
+
     return (
         <tfoot className="border-collapse border border-gray-900 bg-emerald-500">
-        <tr className="border-collapse border border-gray-900" key={props.report.length + 1}>
-            <th className="border-collapse border border-gray-900">Totals</th>
-            {props.hourlyTotal.map(sum => (<th className="border-collapse border border-gray-900">{sum}</th>))}
-            <th className="border-collapse border border-gray-900">{props.hourlyTotal.reduce((acc, curr) => { acc = acc + curr; return acc }, 0)}</th>
-        </tr>
-    </tfoot>
+            <tr className="border-collapse border border-gray-900">
+                <th className="border-collapse border border-gray-900">Totals</th>
+                {totalSumArray.map(sum => (<th className="border-collapse border border-gray-900">{sum}</th>))}
+                <th className="border-collapse border border-gray-900">{totalSumArray.reduce((acc, curr) => { acc = acc + curr; return acc }, 0)}</th>
+            </tr>
+        </tfoot>
     )
 }
