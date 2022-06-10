50777aaaa23e0706a59e3bd79cdd41c8e4e57ca1
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-09 18:55:37 -0700

    simplified and cleaned form data, also rematched fields to match API

--- components/SubmissionForm.js
+++ components/SubmissionForm.js
@@ -1,31 +1,24 @@
-import { useState } from 'react'
+import { useAuth } from '../contexts/auth';
+import useResource from '../hooks/useResource';
 
 const SubmissionForm = (props) => {
 
-    const initValues = {
-        location: '',
-        minCustomers: 0,
-        maxCustomers: 0,
-        avgCookies: 0.00,
-    };
-    
-    const [values, setValues] = useState(initValues);
-    
-    function submitHandler(e) {
-        e.preventDefault();
-        props.createData(values);
+    const { user } = useAuth();
+    const { createResource } = useResource();
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const stand_data = {
+            location: event.target.location.value,
+            minimum_customers_per_hour: parseInt(event.target.minimum.value),
+            maximum_customers_per_hour: parseInt(event.target.maximum.value),
+            average_cookies_per_sale: parseFloat(event.target.average.value),
+            owner: user.id,
+        };
+        createResource(stand_data);
         props.setNumLoc()
-        setValues(initValues)
     }
-    
-    function changeHandler(e) {
-        let { name, value, type } = e.target;
-        if (type === "number") {
-            value = parseFloat(value);
-        }
-        setValues({ ...values, [name]: value });
-        console.log(values)
-    }
+
     
     return (
         <>
@@ -35,22 +28,22 @@ const SubmissionForm = (props) => {
                     <div className='px-2'>
                         <div className="flex mb-6 items-center">
                             <label className='pr-2'>Location</label>
-                            <input className="w-full p-4 mt-3 h-2" type="text" name="location" id="location" value={values.location} onChange={changeHandler} placeholder="Cookie Stand Location" />
+                            <input className="w-5/6 p-3 my-2 h-6" placeholder='location' name='location' />
                         </div>
                     </div>
 
                     <div className="flex gap-x-4 px-2">
                         <div className='bg-emerald-200 rounded'>
                             <label className='text-sm'>Minimum Customers per Hour</label>
-                            <input className="w-5/6 p-3 my-2 h-6" type="number" name="minCustomers" id="minCustomers" value={values.minCustomers} onChange={changeHandler} placeholder="1"/>
+                            <input className="w-5/6 p-3 my-2 h-6" placeholder='minimum' name='minimum' />
                         </div>
                         <div className='bg-emerald-200 rounded'>
                             <label className='text-sm'>Maximum Customers per Hour</label>
-                            <input className="w-5/6 p-3 my-2 h-6" type="number" name="maxCustomers" id="maxCustomers" value={values.maxCustomers} onChange={changeHandler} />
+                            <input className="w-5/6 p-3 my-2 h-6" placeholder='maximum' name='maximum' />
                         </div>
                         <div className='bg-emerald-200 rounded'>
                             <label className='text-sm'>Average Cookies per Sale</label>
-                            <input className="w-5/6 p-3 my-2 h-6" type="number" step='0.01' name="avgCookies" id="avgCookies" value={values.avgCookies} onChange={changeHandler} />
+                            <input className="w-5/6 p-3 my-2 h-6" placeholder='average' name='average' />
                         </div>
                     <button type="submit" className="w-2/5 bg-emerald-500 rounded">Create</button>
                     </div>
