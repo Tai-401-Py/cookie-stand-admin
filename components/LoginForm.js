321882c4fc1cd252ce41944a63e6014913c0130e
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-09 18:55:11 -0700

    added and styled loginform

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,3 +1,4 @@
+
 const LoginForm = ({ onLogin }) => {
 
   async function handleSubmit(event) {
@@ -6,16 +7,18 @@ const LoginForm = ({ onLogin }) => {
   }
 
   return (
-      <form onSubmit={handleSubmit}>
+      <div className="w-full flex justify-center pt-3 ">
+
+      <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4 bg-green-200 rounded shadow-md">
           <fieldset autoComplete='off'>
-              <legend>Log In</legend>
-              <label htmlFor="username">Username</label>
-              <input name="username" />
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" />
-              <button>Log In</button>
+              <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="username">Username</label>
+              <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" name="username" />
+              <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">Password</label>
+              <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="password" name="password" />
+              <button className="px-4 py-2 font-bold text-white bg-green-700 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">Log In</button>
           </fieldset>
       </form>
+      </div>
   );
 }
 
