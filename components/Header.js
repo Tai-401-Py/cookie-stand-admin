7e7f446567f36e78e13492a2fa0bd94383899f7f
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-13 17:18:18 -0700

    added logout

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
+import { logout } from '../contexts/auth';
+
 const Header = () => {
   return (
     <>
       <header className='bg-emerald-500 py-6 pl-6 flex'>
         <h1 className='text-2xl'>Cookie Stand Admin</h1>
+        <button onClick={logout} className='bg-gray-200 hover:bg-gray-500 text-sm border rounded ml-auto mr-6'>Logout</button>
         <button className='bg-gray-200 hover:bg-gray-500 text-sm border rounded ml-auto mr-6'>Overview</button>
       </header>
     </>
