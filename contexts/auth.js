b293d0c728c9d7fda1816b013aaafe0c4c1166f3
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-13 18:20:38 -0700

    hardcoded baseURL

--- contexts/auth.js
+++ contexts/auth.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from 'react';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
-const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const baseUrl = 'https://wild-cookie-api.herokuapp.com'
 const tokenUrl = baseUrl + '/api/token/';
 
 const AuthContext = createContext();
